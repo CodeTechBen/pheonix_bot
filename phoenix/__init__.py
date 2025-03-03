1d9198fac14cdfde4c44dd174b329bd58723346c
CodeTechBen <benjamin-smith@live.com>
2025-03-03 17:28:08 +0000

    removed bad improvements

--- phoenix/__init__.py
+++
@@ -1,2 +0,0 @@
-from .commands import Admin, Character, Creature, Location, Spell
-from .database_utils import DatabaseConnection, DatabaseMapper, DatabaseIDFetch, UserInputHelper, InventoryDatabase, EmbedHelper, DataInserter 
--- phoenix/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-from .admin import Admin
-from .character import Character
-from .creature import Creature
-from .location import Location
-from .spell import Spell
-
-__all__ = ["Admin", "Character", "Creature", "Location", "Spell"]
