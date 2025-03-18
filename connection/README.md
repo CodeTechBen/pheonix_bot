b602eb11a54d13f5ebc50b0aac9104c289379946
CodeTechBen <benjamin-smith@live.com>
2025-03-18 09:16:55 +0000

    altered README to explain how to convert db to global

--- connection/README.md
+++ connection/README.md
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt upgrade
-
+install postgres with `sudo apt install postgresql postgresql-contrib`
 Then git clone this repository using `git clone https://github.com/CodeTechBen/pheonix_bot.git`
+Use the bash script in `connection` run `bash convert-db-to-global.sh`.
--- connection/convert-db-to-global.sh
+++ connection/convert-db-to-global.sh
@@ -4,3 +4,5 @@ sudo sed -i "s/^#\?listen_addresses.*/listen_addresses = '*'/" /etc/postgresql/9
 echo "host    all             all             0.0.0.0/0            md5" | sudo tee -a /etc/postgresql/9.5/main/pg_hba.conf
 
 sudo systemctl restart postgresql
+
+echo "converted psql database to global"
