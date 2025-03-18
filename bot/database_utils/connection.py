34c35653a0580f2073248f8943da553ba802e0fe
CodeTechBen <benjamin-smith@live.com>
2025-03-18 10:58:05 +0000

    connects to the host

--- bot/database_utils/connection.py
+++ bot/database_utils/connection.py
@@ -3,22 +3,34 @@ from os import environ as ENV
 import psycopg2
 from psycopg2.extensions import connection
 from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class DatabaseConnection:
     """Object that returns or closes a connection to the database"""
     _connection = None
-
     @classmethod
     def get_connection(cls) -> connection:
         """Returns a single postgres connection"""
+        # if cls._connection is None:
+        #     cls._connection = psycopg2.connect(
+        #         dbname=ENV['DB_NAME'],
+        #         user=ENV['DB_USER'],
+        #         host=ENV['DB_HOST'],
+        #         port=ENV['DB_PORT'],
+        #         cursor_factory=RealDictCursor)
+        # return cls._connection  # Return the existing connection
         if cls._connection is None:
             cls._connection = psycopg2.connect(
-                dbname=ENV['DB_NAME'],
-                user=ENV['DB_USER'],
-                host=ENV['DB_HOST'],
-                port=ENV['DB_PORT'],
-                cursor_factory=RealDictCursor)
-        return cls._connection  # Return the existing connection
+                dbname=ENV['HOSTED_DB_NAME'],
+                user=ENV['HOSTED_DB_USER'],
+                password=ENV['HOSTED_DB_PASSWORD'],
+                host=ENV['HOSTED_DB_HOST'],
+                port=ENV['HOSTED_PORT'],
+                cursor_factory=RealDictCursor
+            )
+        return cls._connection
 
     @classmethod
     def close_connection(cls):
